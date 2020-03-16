08bdf35a800c2e2cca2e272a5cad09988299a402
AngelGuante <miguel_guante@outlook.com>
2020-03-15 22:51:50 -0400

    Vue y Bootstrap agregados;

---
+++ MonicaExtraWeb/Controllers/SAPController.cs
@@ -0,0 +1,27 @@
+﻿using MonicaExtraWeb.Models.monica10_global;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MonicaExtraWeb.Controllers
+{
+    public class SAPController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SeleccionarEmpresa()
+        {
+            using (var empresas = new monica10_globalEntities())
+            {
+                ViewBag.empresas = JsonConvert.SerializeObject(empresas.empresas.Select(e => new { e.empresa_id, e.Nombre_empresa }).ToList());
+                return View();
+            }
+        }
+    }
+}
