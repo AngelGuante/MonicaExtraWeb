1e270e4f1e668340c7d3cfe8a8cc019fa834a7e4
AngelGuante <miguel_guante@outlook.com>
2020-03-16 21:52:30 -0400

    Se han agregado endpoints para que sea una SAP y se a avanzado hasta llegar a la parte de CRUD de movimientos;

---
+++ MonicaExtraWeb/Controllers/API/APISAPController.cs
@@ -0,0 +1,56 @@
+﻿using MonicaExtraWeb.Models.monica10_global;
+using MonicaExtraWeb.Models.monicaExtra;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace MonicaExtraWeb.Controllers.API
+{
+    [RoutePrefix("API/ASPISAP")]
+    public class APISAPController : ApiController
+    {
+        public empresa Empresa { get; set; } = null;
+
+        /// <summary>
+        /// Retorna todas las empresas almacenadas en la base de datos.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetEmpresas")]
+        public IHttpActionResult GetEmpresas()
+        {
+            using (var empresas = new monica10_globalEntities())
+                return Json(new { Empresas = empresas.empresas.Select(e => new { e.empresa_id, e.Nombre_empresa }).ToList() });
+        }
+
+        /// <summary>
+        /// Guardar los datos del id de la empresa seleccionada.
+        /// </summary>
+        /// <param name="idEmpresa"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("EmpresaSeleccionada")]
+        public IHttpActionResult EmpresaSeleccionada(int idEmpresa)
+        {
+            using (var empresaSeleccionada = new monica10_globalEntities())
+                Empresa = empresaSeleccionada.empresas.Where(e => e.empresa_id == idEmpresa)
+                                                      .FirstOrDefault();
+            if (Empresa == null)
+                return Content(HttpStatusCode.NoContent, "Empresa no encontrada.");
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// Obtener un listado de todos los movimientos hechos de manera decendente.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("ObtenerListadoMovimientos")]
+        public IHttpActionResult ObtenerListadoMovimientos()
+        {
+            using (var movimientos = new monica10Entities())
+                return Json(new { movimientos = movimientos.movimientocajas.ToList().OrderByDescending(m => m.NumeroTransacion) });
+        }
+    }
+}
--- MonicaExtraWeb/Controllers/SAPController.cs
+++ MonicaExtraWeb/Controllers/SAPController.cs
@@ -1,27 +1,10 @@
-﻿using MonicaExtraWeb.Models.monica10_global;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace MonicaExtraWeb.Controllers
 {
     public class SAPController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult SeleccionarEmpresa()
-        {
-            using (var empresas = new monica10_globalEntities())
-            {
-                ViewBag.empresas = JsonConvert.SerializeObject(empresas.empresas.Select(e => new { e.empresa_id, e.Nombre_empresa }).ToList());
-                return View();
-            }
-        }
+        public ActionResult Index() =>
+            View();
     }
 }
